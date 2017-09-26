d1fb6bc879fc34d5f8af5079eb71f4e863692013
gguoss <1536310027@qq.com>
2017-09-26 13:23:18 +0800

    Make sure that bytom the correct execution path

--- test.sh
+++ test.sh
@@ -2,10 +2,10 @@
 
 if [ "$1" = "bytom0" ];
 then
-    ./bytom node --home ./test/.bytom0
+    ../bytom node --home ./test/.bytom0
 elif [ "$1" = "bytom1" ];
 then
-    ./bytom node --home ./test/.bytom1
+    ../bytom node --home ./test/.bytom1
 else
     echo "please cin -----./test.sh bytom0[bytom1]------ ."
 fi
